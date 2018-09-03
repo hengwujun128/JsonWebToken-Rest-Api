c2196ab3f1ffb94ec026bbb92adf4edb3c62a335
天山来客 <hengwujun128@gmail.com>
2018-09-04 00:17:40 +0800

    change  username  of mlab

--- db.js
+++ db.js
@@ -1,4 +1,4 @@
 // The db.js makes sure the application connects to the database.
 
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://hengwujun:hwj147258369@ds053894.mlab.com:53894/rest-api');
+mongoose.connect('mongodb://zhangsan:hwj147258369@ds053894.mlab.com:53894/rest-api');
--- user/UserController.js
+++ user/UserController.js
@@ -2,17 +2,20 @@ var express = require('express');
 var router = express.Router();
 var bodyParser = require('body-parser');
 
-router.use(bodyParser.urlencoded({ extended: true }));
+router.use(bodyParser.urlencoded({
+    extended: true
+}));
 router.use(bodyParser.json());
 var User = require('./User');
 
 // CREATES A NEW USER
 router.post('/', function (req, res) {
+    console.log(req.body)
     User.create({
-            name : req.body.name,
-            email : req.body.email,
-            password : req.body.password
-        }, 
+            name: req.body.name,
+            email: req.body.email,
+            password: req.body.password
+        },
         function (err, user) {
             if (err) return res.status(500).send("There was a problem adding the information to the database.");
             res.status(200).send(user);
@@ -40,13 +43,15 @@ router.get('/:id', function (req, res) {
 router.delete('/:id', function (req, res) {
     User.findByIdAndRemove(req.params.id, function (err, user) {
         if (err) return res.status(500).send("There was a problem deleting the user.");
-        res.status(200).send("User: "+ user.name +" was deleted.");
+        res.status(200).send("User: " + user.name + " was deleted.");
     });
 });
 
 // UPDATES A SINGLE USER IN THE DATABASE
 router.put('/:id', function (req, res) {
-    User.findByIdAndUpdate(req.params.id, req.body, {new: true}, function (err, user) {
+    User.findByIdAndUpdate(req.params.id, req.body, {
+        new: true
+    }, function (err, user) {
         if (err) return res.status(500).send("There was a problem updating the user.");
         res.status(200).send(user);
     });
